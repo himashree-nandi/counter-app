11e70663e604debded46ccb04ff013468e8e15f7
Himashree Nandi <nandihimashree@gmail.com>
2023-04-14 22:35:14 +0530

    modified

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 
 export default function CounterApp() {
-    const[count,setCount]=useState(0)
+  const [count, setCount] = useState(0)
   return (
-    <div><h1>Counter App</h1>
-    <button className='btn btn-primary' onClick={()=>setCount(count+1)}>Increase</button>
-    <p>{count}</p>
-    <button className='btn btn-warning'onClick={()=>setCount(count-1)}>Decrease</button>
-    </div>
+    <>
+      <center>
+        <div className='box'>
+          <h1>Counter App</h1>
+          <button className='btn btn-primary' onClick={() => setCount(count + 1)}>Increase</button>
+          <p>{count}</p>
+          <button className='btn btn-warning' onClick={() => setCount(count - 1)}>Decrease</button>
+        </div>
+      </center>
+    </>
   )
 }
